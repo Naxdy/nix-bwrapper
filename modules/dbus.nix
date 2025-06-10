bc5c3e9912495741fea45584cec01995edf73d83
Clayton <claytongilmer24@outlook.com>
2025-06-09 23:48:13 -0400

    fix(dbus): bwrap commands when flatpak emulation is not enabled but dbus is enabled

--- modules/dbus.nix
+++ modules/dbus.nix
@@ -161,7 +161,7 @@ in
           --new-session \
           --ro-bind /nix /nix \
           --bind "/run" "/run" \
-          ${lib.optionalString config.flatpak.enable ''--ro-bind "$HOME/.bwrapper/${config.app.bwrapPath}/.flatpak-info" "/.flatpak-info" \''}
+          ${lib.optionalString config.flatpak.enable ''--ro-bind "$HOME/.bwrapper/${config.app.bwrapPath}/.flatpak-info" "/.flatpak-info"''} \
           --die-with-parent \
           --clearenv \
           -- \
@@ -173,7 +173,7 @@ in
           --new-session \
           --ro-bind /nix /nix \
           --bind "/run" "/run" \
-          ${lib.optionalString config.flatpak.enable ''--ro-bind "$HOME/.bwrapper/${config.app.bwrapPath}/.flatpak-info" "/.flatpak-info" \''}
+          ${lib.optionalString config.flatpak.enable ''--ro-bind "$HOME/.bwrapper/${config.app.bwrapPath}/.flatpak-info" "/.flatpak-info"''} \
           --die-with-parent \
           --clearenv \
           -- \
