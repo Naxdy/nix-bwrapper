3b0d58d4d3e8da89147369d803926998798443e4
Dominique Hummel <naxdy@naxdy.org>
2025-12-21 13:23:00 +0100

    feat(flatpak): add ability to pre-fill options from manifest file (#25)

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.flatpak;
 in
@@ -9,31 +14,162 @@ in
       default = true;
       description = "Whether to enable certain tricks to make the sandboxed app think it's being run as a flatpak. This is required for portals to work.";
     };
+
+    manifestFile = lib.mkOption {
+      type = lib.types.nullOr lib.types.path;
+      default = null;
+      example = lib.literalExpression ''
+        fetchurl {
+          url = "https://raw.githubusercontent.com/flathub/io.gitlab.librewolf-community/refs/heads/master/io.gitlab.librewolf-community.json";
+          hash = "...";
+        }
+      '';
+      description = ''
+        A path pointing to a flatpak manifest in JSON format.
+
+        If set, will parse the given manifest file and pre-set applicable options in bwrapper wherever possible.
+        In a perfect world, this should make it unnecessary for you to have to customize any other options.
+      '';
+    };
   };
 
-  config = lib.mkIf cfg.enable {
-    assertions = [
+  config = lib.mkIf cfg.enable (
+    lib.mkMerge [
       {
-        assertion = cfg.enable -> config.dbus.enable;
-        message = "Flatpak emulation will do nothing without forwarding the required DBus interfaces.";
+        assertions = [
+          {
+            assertion = cfg.enable -> config.dbus.enable;
+            message = "Flatpak emulation will do nothing without forwarding the required DBus interfaces.";
+          }
+        ];
+
+        fhsenv.bwrap.additionalArgs = [
+          "--ro-bind \"$HOME/.bwrapper/${config.app.bwrapPath}/.flatpak-info\" \"/.flatpak-info\""
+        ];
+
+        # HACK: To make most flatpak apps function correctly, we need a fake `.flatpak-info` file at
+        # the root of the sandbox. Additionally, we need a fake `bwrapinfo.json` file in the host
+        # system. Don't yet know if the values in there should be something sensible, for now just
+        # setting everything to `1` seems to work fine.
+        script.preCmds.stage1 = ''
+          test -f "$HOME/.bwrapper/${config.app.bwrapPath}/.flatpak-info" && rm "$HOME/.bwrapper/${config.app.bwrapPath}/.flatpak-info"
+          mkdir -p "$HOME/.bwrapper/${config.app.bwrapPath}" || { echo 'could not ensure directory under $HOME/.bwrapper'; exit 1; }
+          printf "[Application]\nname=${config.app.id}\n\n[Instance]\ninstance-id = 0\nsystem-bus-proxy = true\nsession-bus-proxy = true\n" > "$HOME/.bwrapper/${config.app.bwrapPath}/.flatpak-info" || { echo 'could not write .flatpak-info'; exit 1; }
+
+          mkdir -p "$XDG_RUNTIME_DIR/.flatpak/0" || { echo 'could not ensure fake flatpak directory'; exit 1; }
+          printf '{"child-pid": 1, "mnt-namespace": 1, "net-namespace": 1, "pid-namespace": 1}' > "$XDG_RUNTIME_DIR/.flatpak/0/bwrapinfo.json" || { echo 'could not write fake bwrapinfo.json'; exit 1; }
+        '';
       }
-    ];
 
-    fhsenv.bwrap.additionalArgs = [
-      "--ro-bind \"$HOME/.bwrapper/${config.app.bwrapPath}/.flatpak-info\" \"/.flatpak-info\""
-    ];
+      (lib.mkIf (cfg.manifestFile != null) (
+        let
+          manifest = builtins.fromJSON (builtins.readFile cfg.manifestFile);
 
-    # HACK: To make most flatpak apps function correctly, we need a fake `.flatpak-info` file at
-    # the root of the sandbox. Additionally, we need a fake `bwrapinfo.json` file in the host
-    # system. Don't yet know if the values in there should be something sensible, for now just
-    # setting everything to `1` seems to work fine.
-    script.preCmds.stage1 = ''
-      test -f "$HOME/.bwrapper/${config.app.bwrapPath}/.flatpak-info" && rm "$HOME/.bwrapper/${config.app.bwrapPath}/.flatpak-info"
-      mkdir -p "$HOME/.bwrapper/${config.app.bwrapPath}" || { echo 'could not ensure directory under $HOME/.bwrapper'; exit 1; }
-      printf "[Application]\nname=${config.app.id}\n\n[Instance]\ninstance-id = 0\nsystem-bus-proxy = true\nsession-bus-proxy = true\n" > "$HOME/.bwrapper/${config.app.bwrapPath}/.flatpak-info" || { echo 'could not write .flatpak-info'; exit 1; }
+          argsOf =
+            type:
+            map (e: lib.removePrefix "--${type}=" e) (
+              builtins.filter (e: lib.hasPrefix "--${type}=" e) manifest.finish-args
+            );
 
-      mkdir -p "$XDG_RUNTIME_DIR/.flatpak/0" || { echo 'could not ensure fake flatpak directory'; exit 1; }
-      printf '{"child-pid": 1, "mnt-namespace": 1, "net-namespace": 1, "pid-namespace": 1}' > "$XDG_RUNTIME_DIR/.flatpak/0/bwrapinfo.json" || { echo 'could not write fake bwrapinfo.json'; exit 1; }
-    '';
-  };
+          hasSocket = name: lib.lists.any (e: e == name) (argsOf "socket");
+          hasShare = name: lib.lists.any (e: e == name) (argsOf "share");
+          hasUnshare = name: lib.lists.any (e: e == name) (argsOf "unshare");
+
+          filesystemSubstitutes =
+            let
+              xdg-user-dirs = "${pkgs.xdg-user-dirs}/bin/xdg-user-dir";
+
+              xdg-dir = dir: "$(${xdg-user-dirs} ${dir})";
+            in
+            {
+              "home/" = "$HOME/";
+              "xdg-desktop" = xdg-dir "DESKTOP";
+              "xdg-documents" = xdg-dir "DOCUMENTS";
+              "xdg-download" = xdg-dir "DOWNLOAD";
+              "xdg-music" = xdg-dir "MUSIC";
+              "xdg-pictures" = xdg-dir "PICTURES";
+              "xdg-public-share" = xdg-dir "PUBLICSHARE";
+              "xdg-templates" = xdg-dir "TEMPLATES";
+              "xdg-videos" = xdg-dir "VIDEOS";
+              "xdg-run" = "$XDG_RUNTIME_DIR";
+              "xdg-config" = "$HOME/.config";
+              "xdg-cache" = "$HOME/.cache";
+              "xdg-data" = "$HOME/.local/share";
+            };
+
+          unsupportedFsSubstitutes = [
+            "host/"
+            "host-os/"
+            "host-etc/"
+          ];
+
+          substituteFsPath =
+            path:
+            builtins.replaceStrings (builtins.attrNames filesystemSubstitutes)
+              (builtins.attrValues filesystemSubstitutes)
+              path;
+        in
+        {
+          app = {
+            inherit (manifest) id;
+
+            env = builtins.listToAttrs (
+              map (
+                e:
+                let
+                  split = lib.splitString "=" e;
+                in
+                {
+                  name = builtins.elemAt split 0;
+                  value = builtins.concatStringsSep "=" (lib.lists.sublist 1 ((builtins.length split) - 1) split);
+                }
+              ) (argsOf "env")
+            );
+          };
+
+          sockets = {
+            pulseaudio = hasSocket "pulseaudio";
+            pipewire = hasSocket "pulseaudio" || hasSocket "pipewire";
+            cups = hasSocket "cups";
+          };
+
+          fhsenv.opts = {
+            unshareNet = !(hasShare "network") || (hasUnshare "network");
+            unshareIpc = !(hasShare "ipc") || (hasUnshare "ipc");
+          };
+
+          mounts = {
+            read = map (e: substituteFsPath (lib.removeSuffix ":ro" e)) (
+              builtins.filter (
+                e: (lib.hasSuffix ":ro" e) && !(lib.lists.any (i: lib.hasInfix i e) unsupportedFsSubstitutes)
+              ) (argsOf "filesystem")
+            );
+            readWrite = map (e: substituteFsPath (lib.removeSuffix ":create" (lib.removeSuffix ":rw" e))) (
+              builtins.filter (
+                e:
+                ((lib.hasSuffix ":rw" e) || (lib.hasSuffix ":create" e))
+                && !(lib.lists.any (i: lib.hasInfix i e) unsupportedFsSubstitutes)
+              ) (argsOf "filesystem")
+            );
+            sandbox = map (e: {
+              name = if (lib.hasPrefix "/" e) then "root/${lib.removePrefix "/" e}" else e;
+              path = if (lib.hasPrefix "/" e) then e else "$HOME/${e}";
+            }) (argsOf "persist");
+          };
+
+          dbus = {
+            session = {
+              talks = argsOf "talk-name";
+              owns = argsOf "own-name";
+              calls = argsOf "call-name";
+            };
+            system = {
+              talks = argsOf "system-talk-name";
+              calls = argsOf "system-call-name";
+            };
+          };
+        }
+      ))
+    ]
+  );
 }
